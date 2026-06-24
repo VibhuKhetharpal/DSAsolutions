081a4d8c1ff49772a04387dd6083f3bde7872381
VibhuKhetharpal <vibhukhetharpal34@gmail.com>
2026-06-24 23:19:25 +0530

    Solved lc 2558

---
+++ leetcode/Heap/2558_TakeGiftsFromtheRichestPile.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<long long> pq;
+        for (int x : gifts) pq.push(x);
+
+        while (k--) {
+            long long x = pq.top();
+            pq.pop();
+            pq.push((long long)sqrt(x));
+        }
+
+        long long ans = 0;
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+        return ans;
+    }
+};
